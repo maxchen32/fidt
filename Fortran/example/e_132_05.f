c395a2aeb9ba80a40d0210d85f05edc984d0f85d
maxchen32 <maxchen32@qq.com>
2020-11-01 22:58:42 +0800

    logo

--- Fortran/example/e_132_05.f
+++
@@ -1,6 +0,0 @@
-      x = 1.
-      y = 2. 
-      z = 3.
-      x y z = x*y*z
-      p   r  i   n  t *, x, y, z, x y             z
-      e n  d
--- Fortran/example/e_141_01.f
+++
@@ -1,7 +0,0 @@
-	a=1.e-38
-	b=1.e-40
-	c=1.e-60
-      print *, a, 1./a
-      print *, b, 1./b
-      print *, c, 1./c
-      END
