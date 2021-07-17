69568d09a563c52097ff85c1f7844fa6f51bb159
maxchen32 <maxchen32@qq.com>
2021-07-17 09:54:47 +0800

    updated09:54:47

--- src_index.py
+++ src_index.py
@@ -3,7 +3,7 @@ srcdir = './src/'
 os.remove(srcdir+"index.md")
 l = os.listdir(srcdir)
 l.sort()
-with open(srcdir+"index.md", mode='w') as f:
+with open(srcdir+"index.md", mode='w', encoding="utf-8") as f:
     for i in l:
         if (i[:4] != "test"):
             f.write('['+i+']('+i+')\n\n')
